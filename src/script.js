55c9995a09baa37bcba5cbb52133efabbfab0c1f
DanielPincu <ghepin01@easv365.dk>
2023-11-16 17:31:19 +0100

    Update JS

--- src/script.js
+++ src/script.js
@@ -4,11 +4,6 @@ function validateForm() {
   var phoneNumber = document.getElementById('exampleInputPhoneNumber').value;
   var agreement = document.getElementById('exampleCheck1').checked;
 
-  if (!email || !fullName || !phoneNumber || !agreement) {
-    alert('Please fill out all fields and agree to the terms.');
-    return false;
-  }
-
   alert('Enrollment successful!');
   return true;
 }
@@ -18,11 +13,7 @@ function validateLoginForm() {
   var password = document.getElementById('exampleInputPassword').value;
   var agreement = document.getElementById('exampleCheck').checked;
 
-  if (!email || !password || !agreement) {
-    alert('Please fill out all fields and agree to the terms.');
-    return false;
-  }
-
   alert('Login successful!');
   return true;
+
 }
