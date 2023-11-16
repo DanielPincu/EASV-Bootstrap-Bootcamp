040de83c0ffa525c2a3f7578c14b001fa8e550bc
DanielPincu <ghepin01@easv365.dk>
2023-11-16 18:33:02 +0100

    Subscribe button OK

--- src/script.js
+++ src/script.js
@@ -17,3 +17,27 @@ function validateLoginForm() {
   return true;
 
 }
+
+function isValidEmail(email) {
+  // Simple email validation regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function subscribeNewsletter() {
+  const emailInput = document.getElementById('emailInput');
+  const email = emailInput.value;
+
+  if (!isValidEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
+  }
+
+  // Perform subscription logic here (e.g., send the email to your server)
+
+  // Show a pop-up message indicating successful subscription
+  alert('Subscription successful! Thank you for subscribing.');
+
+  // Optionally, clear the input field after successful subscription
+  emailInput.value = '';
+}
